9998d31bc3d90b77c01acca77d8674d955a30bc6
Ken <starnub@outlook.com>
2021-08-15 18:19:58 +1000

    Player Controller & Physics
    Added player movement and physics.

---
+++ Book of Darkness/Assets/Scripts/InputAxes.cs
@@ -0,0 +1,7 @@
+public class InputAxes
+{
+    public const string Horizontal = "Horizontal";
+    public const string Vertical = "Vertical";
+    public const string Jump = "Jump";
+    public const string Crouch = "Crouch";
+}
---
+++ Book of Darkness/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+
+    float hMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    void Update()
+    {
+        hMove = Input.GetAxisRaw(InputAxes.Horizontal) * runSpeed;
+        if (Input.GetButtonDown(InputAxes.Jump))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown(InputAxes.Crouch))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp(InputAxes.Crouch))
+        {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(hMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
