ac7309626c32e429233706c148b2fe19807613f3
Ken <starnub@outlook.com>
2021-08-23 18:46:46 +1000

    Added and implemented custom AI script

---
+++ Book of Darkness/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform target;
+
+    public float speed = 2000f;
+    public float nextDist = 3f;
+
+    public Transform enemyGFX;
+
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEnd = false;
+
+    Seeker seeker;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, .5f);
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (path == null)
+        {
+            return;
+        }
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEnd = true;
+            return;
+        }
+        else
+        {
+            reachedEnd = false;
+        }
+
+        Vector2 dir = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = dir * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float dist = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+        if (dist < nextDist)
+        {
+            currentWaypoint++;
+        }
+
+        if (force.x >= 0.01f)
+        {
+            enemyGFX.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (force.x <= -0.01f)
+        {
+            enemyGFX.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+}
