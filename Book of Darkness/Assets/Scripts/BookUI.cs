fb0bf4840ffa3f362786032778f034f0f0c81880
Ken <starnub@outlook.com>
2021-10-26 22:48:57 +1100

    Added book UI visibility based on item in inventory

--- Book of Darkness/Assets/Scripts/BookUI.cs
+++ Book of Darkness/Assets/Scripts/BookUI.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BookUI : MonoBehaviour
 {
     DimensionController dimension;
     Animator animator;
+    Image sprite;
 
     void Start()
     {
         dimension = DimensionController.instance;
         dimension.DimensionSwitchEvent += Transition;
         animator = GetComponent<Animator>();
+        sprite = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        sprite.enabled = false;
+        if (Inventory.instance.HasItem("Book"))
+            sprite.enabled = true;
     }
 
     void Transition()
