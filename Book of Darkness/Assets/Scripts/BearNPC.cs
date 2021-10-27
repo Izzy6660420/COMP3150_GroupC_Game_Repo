dd079baf8c6fd68b627d434053c0ffddbfadb80b
Ken <starnub@outlook.com>
2021-10-28 10:53:04 +1100

    Made generic NPC script and BearNPC extension

---
+++ Book of Darkness/Assets/Scripts/BearNPC.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearNPC : NPC
+{
+    public Item requiredItem;
+    public ItemGroup itemGroup;
+
+    bool recievedItem = false;
+    bool moved = false;
+
+    public Transform target;
+
+    public override void Interact(Collider2D col)
+    {
+        int i = 0;
+        if (talkedTo)
+        {
+            if (requiredItem != null)
+            {
+                if (Inventory.instance.HasItem(requiredItem.name))
+                {
+                    i = 2;
+
+                    if (!moved)
+                    {
+                        recievedItem = true;
+                        Inventory.instance.Remove(requiredItem);
+                        StartCoroutine(Move());
+                    }
+                }
+                else
+                {
+                    var hasItemFromGroup = false;
+                    foreach (Food item in itemGroup.items)
+                    {
+                        if (Inventory.instance.HasItem(item.item.name))
+                            hasItemFromGroup = true;
+                    }
+
+                    i = hasItemFromGroup ? 1 : 3;
+                }
+            }
+        }
+        else
+        {
+            i = 0;
+        }
+
+        if (recievedItem && moved)
+            i = 4;
+        Talk(dialogues[i]);
+    }
+
+    void Talk(Dialogue dialogue)
+    {
+        DialogueManager.instance.StartDialogue(dialogue);
+        talkedTo = true;
+    }
+
+    IEnumerator Move()
+    {
+        float step = 10 * Time.deltaTime;
+
+        while (Vector3.Distance(transform.position, target.position) > 0.01f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+            yield return new WaitForSeconds(10f * Time.deltaTime);
+        }
+
+        moved = true;
+        yield return null;
+    }
+}
--- Book of Darkness/Assets/Scripts/NPC.cs
+++ Book of Darkness/Assets/Scripts/NPC.cs
@@ -5,73 +5,16 @@ using UnityEngine;
 public class NPC : Interactable
 {
     public Dialogue[] dialogues;
-    public Item requiredItem;
-    public ItemGroup itemGroup;
-
-    bool talkedTo = false;
-    bool recievedItem = false;
-    bool moved = false;
-
-    public Transform target;
+    public bool talkedTo;
 
     public override void Interact(Collider2D col)
     {
-        int i = 0;
-        if (talkedTo)
-        {
-            if (requiredItem != null)
-            {
-                if (Inventory.instance.HasItem(requiredItem.name))
-                {
-                    i = 2;
-
-                    if (!moved)
-                    {
-                        recievedItem = true;
-                        Inventory.instance.Remove(requiredItem);
-                        StartCoroutine(Move());
-                    }
-                }
-                else
-                {
-                    var hasItemFromGroup = false;
-                    foreach (Food item in itemGroup.items)
-                    {
-                        if (Inventory.instance.HasItem(item.item.name))
-                            hasItemFromGroup = true;
-                    }
-
-                    i = hasItemFromGroup ? 1 : 3;
-                }
-            }
-        }
-        else
-        {
-            i = 0;
-        }
-
-        if (recievedItem && moved)
-            i = 4;
-        Talk(dialogues[i]);
+        
     }
 
     void Talk(Dialogue dialogue)
     {
         DialogueManager.instance.StartDialogue(dialogue);
         talkedTo = true;
-    }
-
-    IEnumerator Move()
-    {
-        float step = 10 * Time.deltaTime;
-
-        while (Vector3.Distance(transform.position, target.position) > 0.01f)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
-            yield return new WaitForSeconds(10f * Time.deltaTime);
-        }
-
-        moved = true;
-        yield return null;
     }
 }
